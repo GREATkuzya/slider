f4a4040065d4abd4d4fd7b015e989bafa7ba7c5a
GREATkuzya <greatkuzya@gmail.com>
2021-12-16 00:54:37 +0200

    make links and description

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const next = document.querySelector(".arrow-right");
 const prev = document.querySelector(".arrow-left");
 const sliderDots = document.querySelectorAll(".dot");
 const linkAdmiral = document.querySelector(".link-admiral");
+const linkThieves = document.querySelector(".link-thieves");
+const linkPatriotic = document.querySelector(".link-patriotic");
+const linkHotels = document.querySelectorAll(".link-hotels");
+const admiralDesk = document.querySelectorAll(".admiral-desc");
+const thievesDesk = document.querySelectorAll(".thieves-desc");
+const patrioticDesk = document.querySelectorAll(".patriotic-desc");
 let slideIndex = 1;
 let dotIndex = 1;
 
 showSlides(slideIndex);
 initDots(dotIndex);
-linkConnect(slideIndex)
+linkConnect(slideIndex);
+showDescription(slideIndex);
 
 function showSlides(n) {
     if (n > slides.length) {
@@ -19,6 +26,8 @@ function showSlides(n) {
     }
     slides.forEach(item => item.style.display = "none");
     slides[slideIndex - 1].style.display = "";    
+    linkConnect(slideIndex);
+    showDescription(slideIndex);
 }
 
 function nextSlides(n) {
@@ -36,7 +45,7 @@ function initDots(n) {
     sliderDots.forEach(item => item.style.opacity = "0.3");
     sliderDots[dotIndex -1].style.opacity = "1";
 }
-
+// функция возможно лишняя, но не получается результат через другие функции
 function dotClick(index) {
     slides.forEach(item => item.style.display = "none");
     slides[index].style.display = "";    
@@ -44,19 +53,43 @@ function dotClick(index) {
     sliderDots[index].style.opacity = "1";
     slideIndex = index+1;
     dotIndex = index +1;
+    linkConnect(slideIndex);
+    showDescription(slideIndex);
 }
+
+
+// текстовое описание
+function showDescription(n) {
+    if (n === 1) {
+        admiralDesk.forEach(item => item.style.display = "");
+        thievesDesk.forEach(item => item.style.display = "none");
+        patrioticDesk.forEach(item => item.style.display = "none");
+    } if 
+        (n === 2) {
+            admiralDesk.forEach(item => item.style.display = "none");
+            thievesDesk.forEach(item => item.style.display = "");
+            patrioticDesk.forEach(item => item.style.display = "none");
+    }
+      if 
+        (n === 3) {
+            admiralDesk.forEach(item => item.style.display = "none");
+            thievesDesk.forEach(item => item.style.display = "none");
+            patrioticDesk.forEach(item => item.style.display = "");
+    }
+}
+
 next.addEventListener("click", () => {
-    nextSlides(1);
-   console.log(slideIndex, dotIndex);
- });
+    nextSlides(1); 
+});
 
 prev.addEventListener("click", () => {
     nextSlides(-1);
-    console.log(slideIndex, dotIndex);
 });
 
+
+// переключение  по точкам, надо упростить как то одним выражением, через индекс
 sliderDots[1].addEventListener("click", () => {
-    // showSlides(2);
+    // showSlides(2);  по идее эти 2 функции должны делать то же, что и эта оставшаяся, но они не отрабатывают, незнаю почему
     // initDots(2);
     dotClick(1);
     console.log(slideIndex, dotIndex);
@@ -76,11 +109,35 @@ sliderDots[2].addEventListener("click", () => {
     console.log(slideIndex, dotIndex);
 })
 
-function linkConnect(slideIndex) {
-    if (slideIndex === 1) {
-        linkAdmiral.classList.toggle("special-color");
-      linkAdmiral.style.color = "#E3B873";
-    } else {
-        linkAdmiral.style.color = "";
+// переключение  по ссылкам, надо упростить как то одним выражением, через индекс
+linkAdmiral.addEventListener("click", () =>{
+    dotClick(0);
+    linkConnect(slideIndex);
+    showDescription(slideIndex);
+})
+
+linkThieves.addEventListener("click", () =>{
+    dotClick(1);
+    linkConnect(slideIndex);
+    showDescription(slideIndex);
+})
+
+linkPatriotic.addEventListener("click", () =>{
+    dotClick(2);
+    linkConnect(slideIndex);
+    showDescription(slideIndex);
+})
+
+// делает ссылкам нужный стиль
+function linkConnect(n) {
+    if (n > linkHotels.length) {
+        slideIndex = 1;
     }
+    if (n < 1) {
+        slideIndex = linkHotels.length;
+    }
+    linkHotels.forEach(item => item.style.color = "");
+    linkHotels.forEach(item => item.style.textDecorationLine = "none");
+    linkHotels[slideIndex - 1].style.color = "#E3B873";    
+    linkHotels[slideIndex - 1].style.textDecorationLine = "underline";
 }
